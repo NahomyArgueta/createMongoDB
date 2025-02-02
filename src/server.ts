7e0b3b5b121c8de6514925e7c9f8fecec945817c
santiago <santiago.fernandez-pedrera@gracia.lasalle.cat>
2025-02-02 20:33:19 +0100

    Configuración inicial: Servidor Express y conexión a MongoDB Atlas

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,18 @@
 import express from "express";
 import { connectDB } from "./config/db";
 
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-import dotenv from "dotenv";
-dotenv.config();
-
-// Conectar a la base de datos
+// Conectar a MongoDB Atlas
 connectDB();
 
-// Middleware para parsear JSON
 app.use(express.json());
 
-// Ruta básica de prueba
 app.get("/", (req, res) => {
     res.send("¡Servidor funcionando!");
 });
 
-// Iniciar el servidor
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
